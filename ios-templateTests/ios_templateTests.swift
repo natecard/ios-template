820a2844dca32f52635d6fc900ea62d92a19f4ee
Nate Card <ncard24@gmail.com>
2025-11-06 13:25:27 -0500

    Initial Commit

---
+++ ios-templateTests/ios_templateTests.swift
@@ -0,0 +1,16 @@
+//
+//  ios_templateTests.swift
+//  ios-templateTests
+//
+//  Created by Nate Card on 2025-11-06.
+//
+
+import Testing
+
+struct ios_templateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
